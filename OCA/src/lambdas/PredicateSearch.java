fe64cf38e2efef3e8ccd32f5c4a8fe1520df909d
Matt Reis <matt_reis@yahoo.com>
2017-07-18 11:33:31 +0100

    predicate search

--- OCA/src/lambdas/PredicateSearch.java
+++ OCA/src/lambdas/PredicateSearch.java
@@ -2,8 +2,9 @@ package lambdas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
-public class TraditionalSearch {
+public class PredicateSearch {
 
 	public static void main(String[] args) {
 		List<Animal> animals = new ArrayList<>();
@@ -12,11 +13,10 @@ public class TraditionalSearch {
 		animals.add(new Animal("rabbit", true, false));
 		animals.add(new Animal("turtle", false, true));
 		
-		print(animals, new CheckIfHopper());
 		print(animals, a -> a.isHopper());
 	}
 
-	private static void print(List<Animal> animals, CheckTrait checkIfHopper) {
+	private static void print(List<Animal> animals, Predicate<Animal> checkIfHopper) {
 		for(Animal animal : animals) {
 			if(checkIfHopper.test(animal)) {
 				System.out.print(animal + " ");
